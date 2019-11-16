fd45c0341da1cd8ae0eb8df0614679c915741f7e
AceIsHuman <menoman1995@yahoo.com>
2019-11-16 15:22:24 -0600

    create endpoint to create a new car entry

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require("express");
+const carsTable = require("./data/dbConfig");
 
 const server = express();
 
 server.use(express.json());
+
+// ADD A CAR RECORD
+server.post("/api/cars", async (req, res) => {
+  try {
+    const id = await carsTable("cars").insert(req.body);
+    res.status(200).json(id);
+  } catch (error) {
+    res.status(500).json({ errorMessage: "Error adding car", error});
+  }
+});
 
 // Middleware
 
