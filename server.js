17a7c091a910ecb11a156411ef559e44fcfb5cc9
AceIsHuman <menoman1995@yahoo.com>
2019-11-16 15:21:00 -0600

    create validateCarsBody middleware

--- server.js
+++ server.js
@@ -4,4 +4,13 @@ const server = express();
 
 server.use(express.json());
 
+// Middleware
+
+function validateCarsBody(req, res, next) {
+  const { vin, make, model, mileage } = req.body;
+  !vin || !make || !model || !mileage
+    ? res.status(400).json({ message: "vin, make, model, and mileage required" })
+    : next()
+}
+
 module.exports = server;
